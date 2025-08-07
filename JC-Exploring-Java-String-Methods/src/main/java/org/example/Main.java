3699e208b90b7c8c740f886467cc7cd7ab2546e3
Tyrell Teppang <tyrellmarcus.teppang@gcash.com>
2025-08-07 14:49:25 +0800

    Exercises 1 and 2 done

---
+++ JC-Exploring-Java-String-Methods/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Exercise 2: charAt() - Accessing a Character
+        String message = "Keep Learning!";
+        char character = message.charAt(5);
+        System.out.println("The char at index 5 is: " + character);
+
+        //Prediction: The char at index 5 is: L
+        //Actual:
+    }
+}
